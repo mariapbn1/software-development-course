2f4debbc77de8fb2c3b796075c229dcfe186d67d
MariaPaulaBN <mpaulabn1@gmail.com>
2026-02-19 23:06:12 -0500

    feat: create models for the product catalog
    - Implement the core Product model for mobile device management.
    - Add supporting models for brands, colors, supported networks, and operating systems to ensure data integrity.

--- backend/api/products/models.py
+++ backend/api/products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class MaxSupportNetwork(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class OperatingSystem(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    storage = models.IntegerField()
+    ram = models.IntegerField()
+    release_date = models.DateField()
+    max_battery = models.IntegerField()
+    main_camera_res = models.FloatField()
+    selfie_camera_res = models.FloatField()
+    has_nfc = models.BooleanField(default=False)
+    has_headphone_jack = models.BooleanField(default=True)
+    product_image = models.URLField(max_length=250)
+    synopsis = models.TextField()
+
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    max_supported_network = models.ForeignKey(MaxSupportNetwork, on_delete=models.CASCADE)
+    operating_system = models.ForeignKey(OperatingSystem, on_delete=models.CASCADE)
