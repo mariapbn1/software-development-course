aaecf69764abe6037e65e87e1590093da81d08fb
MariaPaulaBN <mpaulabn1@gmail.com>
2026-03-09 23:44:50 -0500

    feat(reports): refine field selection for Product PDF reports
    Adjusted the number of variables exposed in the PDF export to improve readability.

--- backend/api/reports/views.py
+++ backend/api/reports/views.py
@@ -2,8 +2,9 @@ import openpyxl
 
 from rest_framework.views import APIView
 from django.http import HttpResponse, HttpResponseBadRequest
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
 from reportlab.lib import colors
 
 from api.comments.models import Comment
@@ -16,29 +17,39 @@ class DynamicReportView(APIView):
         file_format = request.query_params.get('formate')
 
         if not report_type or not file_format:
-            return HttpResponseBadRequest("Debes especificar 'tipo' (productos/comentarios) y 'formato' (pdf/excel)")
+            return HttpResponseBadRequest("You must specify 'type' (products/comments) and 'formate' (pdf/Excel)")
 
-        if report_type == 'productos':
-            headers = ['Nombre', 'Marca', 'RAM', 'Almacenamiento']
+        if report_type == 'products':
+            headers = [
+                'Nombre', 'Almacenamiento', 'RAM', 'Fecha de Lanzamiento', 'Batería',
+                'Resolución Cámara Principal', 'Resolución Cámara Selfie', 'NFC', 'Conector para Auriculares',
+                'Sinópsis', 'Marca', 'Color', 'Red', 'Sistema Operativo'
+            ]
             queryset = Product.objects.all().select_related('brand')
-            data = [[p.name, p.brand.name, p.ram, p.storage] for p in queryset]
+            data = [
+                [
+                    p.name, p.storage, p.ram, p.release_date, p.max_battery, p.main_camera_res,
+                    p.selfie_camera_res, p.has_nfc, p.has_headphone_jack, p.synopsis,
+                    p.brand.name, p.color.name, p.max_supported_network.name, p.operating_system.name
+                ] for p in queryset]
+
             filename = "reporte_productos"
 
-        elif report_type == 'comentarios':
+        elif report_type == 'comments':
             headers = ['Producto', 'Usuario', 'Fecha', 'Comentario']
             queryset = Comment.objects.all().select_related('product', 'user')
-            data = [[c.product.name, c.user.username, str(c.date), c.comment[:30]] for c in queryset]
+            data = [[c.product.name, c.user.username, c.user.email, str(c.date), c.comment[:30]] for c in queryset]
             filename = "reporte_comentarios"
 
         else:
-            return HttpResponseBadRequest("Tipo de reporte no válido")
+            return HttpResponseBadRequest("Invalid type of report")
 
         if file_format == 'excel':
             return self.generate_excel(headers, data, filename)
         elif file_format == 'pdf':
             return self.generate_pdf(headers, data, filename)
         else:
-            return HttpResponseBadRequest("Formato no soportado (usa 'pdf' o 'excel')")
+            return HttpResponseBadRequest("Format not supported (use 'pdf' or 'excel')")
 
     def generate_excel(self, headers, data, filename):
         wb = openpyxl.Workbook()
@@ -54,17 +65,47 @@ class DynamicReportView(APIView):
 
     def generate_pdf(self, headers, data, filename):
         response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = f'attachment; filename="reports/{filename}.pdf"'
+        response['Content-Disposition'] = f'attachment; filename="{filename}.pdf"'
 
-        doc = SimpleDocTemplate(response, pagesize=letter)
-        full_data = [headers] + data
+        doc = SimpleDocTemplate(response, pagesize=landscape(letter))
+        doc.title = filename
+        elements = []
 
-        table = Table(full_data)
+        styles = getSampleStyleSheet()
+        style_body = styles["BodyText"]
+        style_body.fontSize = 7
+
+        style_header = styles["Heading4"]
+        style_header.fontSize = 8
+        style_header.textColor = colors.whitesmoke
+
+        processed_data = []
+
+        processed_headers = [Paragraph(h, style_header) for h in headers]
+        processed_data.append(processed_headers)
+
+        for row in data:
+            new_row = []
+            for i, item in enumerate(row):
+                if i == 9:
+                    new_row.append(Paragraph(str(item), style_body))
+                elif isinstance(item, bool):
+                    new_row.append("Sí" if item else "No")
+                else:
+                    new_row.append(Paragraph(str(item), style_body))
+            processed_data.append(new_row)
+
+        table = Table(processed_data, repeatRows=1)
+
         table.setStyle(TableStyle([
             ('BACKGROUND', (0, 0), (-1, 0), colors.darkblue),
-            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-            ('GRID', (0, 0), (-1, -1), 0.5, colors.grey),
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+            ('GRID', (0, 0), (-1, -1), 0.3, colors.grey),
+            ('LEFTPADDING', (0, 0), (-1, -1), 3),
+            ('RIGHTPADDING', (0, 0), (-1, -1), 3),
         ]))
 
-        doc.build([table])
+        elements.append(table)
+        doc.build(elements)
         return response
